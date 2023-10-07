1a7433a90f39d5725eeb96f312704873a1484a9a
Roman Savin <135424483+SaR0man@users.noreply.github.com>
2023-10-08 00:39:17 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void input() {
+        System.out.println("Введите выражение");
+        Scanner scanner = new Scanner(System.in);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Введите выражение");
     }
 }
